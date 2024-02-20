0f9c1bdcaebb73d3b59cc8f08afb8773c97f6432
Kasper Welbers <kasperwelbers@gmail.com>
2024-02-20 12:01:30 +0100

    max width

--- src/framework/visualisation/react/main.tsx
+++ src/framework/visualisation/react/main.tsx
@@ -1,15 +1,17 @@
-import * as React from 'react'
+import * as React from "react";
 
 interface MainProps {
-  elements: JSX.Element[]
+  elements: JSX.Element[];
 }
 
 export const Main = ({ elements }: MainProps): JSX.Element => {
-  elements = elements.map((element, index) => { return { ...element, key: `${index}` } })
+  elements = elements.map((element, index) => {
+    return { ...element, key: `${index}` };
+  });
 
   return (
-    <div className='w-full h-full'>
-      {elements}
+    <div className="flex justify-start w-full h-full">
+      <div className="max-w-7xl">{elements}</div>
     </div>
-  )
-}
+  );
+};
